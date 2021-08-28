d6ca0ba07871da0ad7026ec29e4ce2b4b7bbb3bc
caojiayuan <cjy632258@hotmail.com>
2021-08-28 12:07:37 +0800

    load .env

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/enorith/enorith/config"
 	"github.com/enorith/enorith/internal/app"
@@ -10,6 +11,7 @@ import (
 	"github.com/enorith/framework"
 	"github.com/enorith/http"
 	"github.com/enorith/http/router"
+	"github.com/joho/godotenv"
 )
 
 const ServeAt = ":3113"
@@ -18,20 +20,21 @@ func main() {
 	application := framework.NewApp(config.FS)
 	app.BootstrapApp(application)
 
+
+	if application.GetConfig().Env != "production" {
+		cwd, _ := os.Getwd()
+		env := filepath.Join(cwd, ".env")
+		log.Printf("loading .env [%s]", env)
+		_ = godotenv.Load(env)		
+	}
+
 	e := application.Run(ServeAt, func(rw *router.Wrapper, k *http.Kernel) {
 		k.OutputLog = true
 		routes.WebRoutes(rw)
 		rw.Group(func(r *router.Wrapper) {
 			routes.ApiRoutes(r)
 		}, "api")
-
-		for k2, v := range rw.Routes() {
-			for i, pr := range v {
-				fmt.Println(k2, i, pr.Middleware(), pr.Path())
-			}
-		}
 	})
-
 	if e != nil {
 		log.Fatal(e)
 	}
