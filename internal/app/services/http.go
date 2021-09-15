7132baf83707625028219ab49957434d1b65edb0
CaoJiayuan <cjy632258@hotmail.com>
2021-09-15 14:58:35 +0800

    update framework

--- internal/app/services/http.go
+++ internal/app/services/http.go
@@ -5,7 +5,6 @@ import (
 	"github.com/enorith/enorith/internal/app/routes"
 	"github.com/enorith/framework"
 	"github.com/enorith/framework/http/middleware"
-	"github.com/enorith/http/contracts"
 	"github.com/enorith/http/router"
 )
 
@@ -16,6 +15,12 @@ type HttpService struct {
 // you can configure service, prepare global vars etc.
 // running at main goroutine
 func (hs HttpService) Register(app *framework.App) error {
+
+	app.Bind(func(ioc container.Interface) {
+		ioc.BindFunc("middleware.throttle.api", func(c container.Interface) (interface{}, error) {
+			return middleware.Throttle(1, 60), nil
+		}, false)
+	})
 
 	return nil
 }
@@ -28,13 +33,4 @@ func (hs HttpService) RegisterRoutes(rw *router.Wrapper) {
 	rw.Group(func(r *router.Wrapper) {
 		routes.ApiRoutes(r)
 	}, "api").Middleware("throttle.api")
-}
-
-//Lifetime container callback
-// usually register request lifetime instance to IoC-Container (per-request unique)
-// this function will run before every request handling
-func (hs HttpService) Lifetime(ioc container.Interface, request contracts.RequestContract) {
-	ioc.BindFunc("middleware.throttle.api", func(c container.Interface) (interface{}, error) {
-		return middleware.Throttle(1, 60), nil
-	}, false)
 }
