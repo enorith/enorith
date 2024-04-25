eafe3204ab450a7ecf51072e80ea197c791c345d
CaoJiayuan <cjy632258@hotmail.com>
2024-04-25 17:07:02 +0800

    update schedule

--- internal/app/services/schedule.go
+++ internal/app/services/schedule.go
@@ -1,14 +1,20 @@
 package services
 
 import (
+	"strings"
+	"time"
+
+	"github.com/alexeyco/simpletable"
 	"github.com/enorith/framework"
+	"github.com/enorith/framework/crond"
+	"github.com/enorith/supports/carbon"
 	"github.com/go-co-op/gocron"
 )
 
 type ScheduleService struct {
 }
 
-//Register service when app starting, before http server start
+// Register service when app starting, before http server start
 // you can configure service, prepare global vars etc.
 // running at main goroutine
 func (s ScheduleService) Register(app *framework.App) error {
@@ -19,6 +25,37 @@ func (s ScheduleService) Register(app *framework.App) error {
 		// 	fmt.Println(time.Now(), "scheduled")
 		// })
 	})
+	app.Daemon(func(exit chan struct{}) {
+		time.Sleep(time.Second)
+		printSchedules(crond.Scheduler)
+		<-exit
+	})
 
 	return nil
+}
+
+func printSchedules(s *gocron.Scheduler) {
+
+	table := simpletable.New()
+	table.Header = &simpletable.Header{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Text: "Tags"},
+			{Align: simpletable.AlignCenter, Text: "Next"},
+			{Align: simpletable.AlignCenter, Text: "Last"},
+		},
+	}
+	for _, j := range s.Jobs() {
+		table.Body.Cells = append(table.Body.Cells, []*simpletable.Cell{
+			{Align: simpletable.AlignLeft, Text: strings.Join(j.Tags(), ",")},
+			{Align: simpletable.AlignLeft, Text: j.NextRun().Format(carbon.DefaultDateTimeFormat)},
+			{Align: simpletable.AlignLeft, Text: j.LastRun().Format(carbon.DefaultDateTimeFormat)},
+		})
+	}
+	table.Footer = &simpletable.Footer{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Text: "Cron schedules", Span: 3},
+		},
+	}
+	table.SetStyle(simpletable.StyleUnicode)
+	table.Println()
 }
